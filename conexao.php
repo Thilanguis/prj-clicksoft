f6d1ab960bf221240fc026f3ee1979e6ea851f8e
Gabriel Tavares <gabrielt2005@hotmail.com>
2019-02-03 00:05:57 -0200

    Ajustando o banco servidor online

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
 <?php
-   $con = mysqli_connect("localhost", "root", "", "agenda");
+   $con = mysqli_connect("us-cdbr-iron-east-03.cleardb.net","b6e5b0b61fb4ef","c419a1f6","heroku_78be48321e7a85b");
 ?>
